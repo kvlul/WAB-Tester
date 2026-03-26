a337ca7ed655727bd3cac283e5ede67cb73d69d5
Khalil <khvllull@gmail.com>
2026-03-26 23:57:01 +0100

    Rename app.js to app.cjs

---
+++ app.cjs
@@ -0,0 +1,26 @@
+// Old CommonJS
+// const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser'; // ila kayn f app.js
+
+const app = express();
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+app.use(bodyParser.json());
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
